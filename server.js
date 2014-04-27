cad9a960da0a04b723217cbc1e442123ba982760
Kamil Załęski <kamil.zaleski@gmail.com>
2014-04-27 20:49:40 +0200

    0

---
+++ server.js
@@ -0,0 +1,89 @@
+var express = require('express'),
+    app = express(),
+    multer = require('multer');
+
+var Hashids = require("hashids"),
+    hashids = new Hashids("this is my salt", 6);
+
+var redis = require("redis"),
+    client = redis.createClient();
+
+client.on("error", function (err) {
+        console.log("REDIS Error " + err);
+});
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+
+// app.configure(function () {
+    app.use(multer({
+        dest: './tmp/',
+        rename: function (fieldname, filename) {
+            return filename.replace(/\W+/g, '-').toLowerCase() + Date.now();
+        }
+    }));
+    app.use(express.static(__dirname + '/static'));
+// });
+
+app.get('/', function(req,res) {
+    res.render('index')
+});
+
+app.post('/upload', function (req, res) {
+
+
+    file = req.files.shot;
+
+    client.incr('counter',function(err,index) {
+       var new_id = hashids.encrypt(index);
+       
+       client.hmset(new_id,{
+            "path" : file.path,
+            "extension" : file.extension,
+            "mimetype" : file.originalname 
+       },function() {
+
+        res.redirect(301, '/' + new_id);
+
+       });
+
+       console.log('new id',new_id);
+
+
+
+    });
+
+
+});
+
+app.get('/shot/:id', function(req,res) {
+    // client.hgetall(req.params.id,function(er,c) { console.log(c); });
+    
+    client.hget(req.params.id,'path',function(er,c) {
+        res.sendfile(c);
+    });
+
+});
+
+app.get('/:id', function(req,res) {
+    // client.hgetall(req.params.id,function(er,c) { console.log(c); });
+    // client.hget(req.params.id,'path',function(er,c) {
+    //     res.sendfile(c);
+    // });
+
+    var eid = req.params.id;
+
+    client.incr('views-'+eid,function(err, viewCount ) {
+        
+        res.render('shot', {
+            shot : '/shot/' + eid,
+            viewCount : viewCount
+        });
+    });
+    
+});
+
+
+var server = app.listen(3000, function () {
+    console.log('listening on port %d', server.address().port);
+});
